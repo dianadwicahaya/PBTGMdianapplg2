bf99a2d46ca3aae0f216e4cad8fcb0b20889eedc
dianadwicahaya <145527680+dianadwicahaya@users.noreply.github.com>
2024-07-31 12:29:03 +0700

    Diana Dwi Cahaya (XI PPLG 2) - pertemuan 2 tugas nomor 1

---
+++ Pertemuan2tugas1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.pertemuan2tugas1;
+
+/**
+ *
+ * @author diana
+ */
+public class Pertemuan2tugas1 {
+
+    public static void main(String[] args) {
+        int a = 50;
+        float b = a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
