3b34d5234848073cba4666ee25ec31f824fcb8c3
dianadwicahaya <145527680+dianadwicahaya@users.noreply.github.com>
2024-07-31 14:20:01 +0700

    Diana Dwi Cahaya (XI PPLG 2) - pertemuan 3 tugas nomor 1

---
+++ Pertemuan3tugas1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan3tugas1;
+
+/**
+ *
+ * @author diana
+ */
+public class Pertemuan3tugas1 {
+
+    public static void main(String[] args) {
+       int a = 9;
+       float fl = 899;
+       char ch = 'A';
+       boolean b = false;
+       String string = "Hello World!";
+       
+       System.out.println("The integer var1 is  " + a);
+       System.out.println("The float var2 is  " + fl);
+       System.out.println("The varchar var3 is  " + ch);
+       System.out.println("The boolean var4 is  " + b);
+       System.out.println("The string var5 is  " + string);
+    } 
+}
