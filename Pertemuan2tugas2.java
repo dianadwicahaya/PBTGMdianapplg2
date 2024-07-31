7a6f9827e64edfd42999f862c4e23390bd4f1a38
dianadwicahaya <145527680+dianadwicahaya@users.noreply.github.com>
2024-07-31 13:21:09 +0700

    Diana Dwi Cahaya (XI PPLG 2) - pertemuan 2 tugas nomor 2

---
+++ Pertemuan2tugas2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan2tugas2;
+
+/**
+ *
+ * @author diana
+ */
+public class Pertemuan2tugas2 {
+
+    public static String Makanan;
+    public static final String Minuman = "Jus Jambu";
+    public static void main(String[] args) {
+    //TODO code application logic here
+    Makanan = "Ayam Geprek ";//mengisi value pada variable Makanan
+    System.out.println("Makanan favorit saya " + Makanan); //memanggil variable Makanan
+    System.out.println("Minuman favorit saya " + Minuman); //memanggil variable Minuman
+}
+}
