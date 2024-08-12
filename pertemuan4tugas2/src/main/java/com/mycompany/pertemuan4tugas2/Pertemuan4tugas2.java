5e03703336f0be3ee1275ca973cc8c0a96871c4a
dianadwicahaya <145527680+dianadwicahaya@users.noreply.github.com>
2024-08-12 13:04:12 +0700

    Diana Dwi Cahaya (XI-PPLG2) - pertemuan 4 soal nomor 2

---
+++ pertemuan4tugas2/src/main/java/com/mycompany/pertemuan4tugas2/Pertemuan4tugas2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan4tugas2;
+
+/**
+ *
+ * @author diana
+ */
+public class Pertemuan4tugas2 {
+
+    public static void main(String[] args) {
+       int x=6;
+       int w=5;
+       int y=8-x++;
+       int z=8-++w;
+       System.out.println("Nilai x: " + x);
+       System.out.println("Nilai w: " + w);
+       System.out.println("Nilai y: " + y);
+       System.out.println("Nilai z: " + z);
+    }
+}
