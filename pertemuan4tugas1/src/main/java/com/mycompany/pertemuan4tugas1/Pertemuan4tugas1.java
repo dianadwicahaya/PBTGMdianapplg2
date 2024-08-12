e776b8fe9786765439f048e3ad75585b8144bb95
dianadwicahaya <145527680+dianadwicahaya@users.noreply.github.com>
2024-08-12 12:52:29 +0700

    Diana Dwi Cahaya (XI-PPLG2) - pertemuan 4 soal nomor 1

---
+++ pertemuan4tugas1/src/main/java/com/mycompany/pertemuan4tugas1/Pertemuan4tugas1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan4tugas1;
+
+/**
+ *
+ * @author diana
+ */
+public class Pertemuan4tugas1 {
+
+    public static void main(String[] args) {
+       int angka1=10;
+       int angka2=3;
+       System.out.println("A + B: " +
+               (angka1 + angka2) );
+       System.out.println("A - B: " +
+               (angka1 - angka2) );
+       System.out.println("A * B: " +
+               (angka1 * angka2) );
+       System.out.println("A / B: " +
+               (angka1 / angka2) );
+       System.out.println("A % B: " +
+               (angka1 % angka2) );
+    }
+}
