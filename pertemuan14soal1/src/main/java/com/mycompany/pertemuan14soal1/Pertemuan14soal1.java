b3ee08e3232324da1ef3e7062ad3895be4508731
dianadwicahaya <145527680+dianadwicahaya@users.noreply.github.com>
2024-10-09 12:19:44 +0700

    Add files via upload

---
+++ pertemuan14soal1/src/main/java/com/mycompany/pertemuan14soal1/Pertemuan14soal1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan14soal1;
+
+/**
+ *
+ * @author diana
+ */
+
+    public class Pertemuan14soal1 {
+    public static void main(String[] args) {
+         // TODO code application logic here
+        abstrakhewan objek = new kucingabstrak();
+        objek.suara();
+        objek.suara2();
+    }
+    
+}
---
+++ pertemuan14soal1/src/main/java/com/mycompany/pertemuan14soal1/kucingabstrak.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pertemuan14soal1;
+
+/**
+ *
+ * @author diana
+ */
+public class kucingabstrak extends abstrakhewan {
+    @Override
+    public void suara() {
+        System.out.println(" Suara kucing mengeong");
+    
+    }
+}  
+
