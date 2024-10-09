b9a061a939336dfedb8a49015ae777d62dc5630d
dianadwicahaya <145527680+dianadwicahaya@users.noreply.github.com>
2024-10-09 10:45:10 +0700

    Add files via upload

---
+++ pertemuan7tugas1/src/main/java/com/mycompany/pertemuan7tugas1/Pertemuan7tugas1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.pertemuan7tugas1;
+
+/**
+ *
+ * @author diana
+ */
+public class Pertemuan7tugas1 {
+
+    public static void main(String[] args) {
+        int day = 2;
+        switch (day) {
+            case 1:
+                System.out.println("Sabtu");
+                break;
+            case 2:
+                System.out.println("Minggu");
+                break;
+        
+        }
+    }
+    
+}
