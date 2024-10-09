62ac59335719a2361935eb1831ca722e563b57aa
dianadwicahaya <145527680+dianadwicahaya@users.noreply.github.com>
2024-10-09 12:20:41 +0700

    Add files via upload

---
+++ pertemuan14soal2/src/main/java/com/mycompany/pertemuan14soal2/Pertemuan14soal2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan14soal2;
+
+/**
+ *
+ * @author diana
+ */
+public class Pertemuan14soal2 {
+    
+    public static void main(String[] args) {
+        abstrakhewan objek = new burung();
+        objek.suara();
+    }
+    
+}
---
+++ pertemuan14soal2/src/main/java/com/mycompany/pertemuan14soal2/burung.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pertemuan14soal2;
+
+/**
+ *
+ * @author diana
+ */
+public class burung extends abstrakhewan {
+    @Override
+    public void suara() {
+        System.out.println("Suara Burung mencicit");
+ 
+    }
+    
+}
