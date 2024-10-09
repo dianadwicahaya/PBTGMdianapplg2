f513dbea2ad37204e5a2f909146caf63ebec3bb8
dianadwicahaya <145527680+dianadwicahaya@users.noreply.github.com>
2024-10-09 11:38:09 +0700

    Add files via upload

---
+++ pertemuan9tugas1/src/main/java/com/mycompany/pertemuan9tugas1/Pertemuan9tugas1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.pertemuan9tugas1;
+/**
+ *
+ * @author liandra
+ */
+public class Pertemuan9tugas1 {
+
+    public static void main(String[] args) {
+       int i = 1;
+       while(i <= 5){
+           int j = 1;
+           while(j <= i){
+               System.out.print("* ");
+               j++;
+           }
+          System.out.println(); 
+          i++;
+       }
+    }
+    
+}
